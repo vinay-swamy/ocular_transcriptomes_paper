df1b5fd8d05699dd22d531abd16f108e864f7734
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2019-12-12 16:27:39 -0500

    variable output files

--- scripts/count_stats_from_files.sh
+++ scripts/count_stats_from_files.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 quant_path=$1
-rm -rf tmp
+DNTX_mapping_rates=$2
+gencode_mappingrates=$3
+gtfs_counts=$4
+rm -f $2 $3 $4
 mkdir -p tmp
 for sample in ${quant_path}/data/salmon_quant/*/*/aux_info/meta_info.json
     do
-        echo $sample `grep num_processed $sample ` `grep percent_mapped $sample` >> tmp/DNTX_mappingrates.tab
+        echo $sample `grep num_processed $sample ` `grep percent_mapped $sample` >> ${DNTX_mapping_rates}
     done
+
 
 for sample in ${quant_path}/gencode_quant/quant_files/*/aux_info/meta_info.json
     do
-        echo $sample `grep num_processed $sample ` `grep percent_mapped $sample` >> tmp/gencode_mappingrates.tab
+        echo $sample `grep num_processed $sample ` `grep percent_mapped $sample` >> ${gencode_mappingrates}
     done
+
 
 for gtf in ${quant_path}/data/gtfs/*/*.gtf
     do
-        echo $gtf `awk '$3 == "transcript"' $gtf | wc -l` >> tmp/gtf_tx_counts.tab
+        echo $gtf `awk '$3 == "transcript"' $gtf | wc -l` >> ${gtfs_counts}
     done
