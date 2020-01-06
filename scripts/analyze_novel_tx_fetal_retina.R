99edf6dbdea9e4b9b92c7f49364f44be3d0c1c8d
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-01-06 11:02:10 -0500

    old results we got for time course scripts

--- scripts/analyze_novel_tx_fetal_retina.R
+++ scripts/analyze_novel_tx_fetal_retina.R
@@ -3,33 +3,35 @@ library(limma)
 library(edgeR)
 library(ComplexHeatmap)
 library(viridis)
-args <- c('~/NIH/occular_transcriptomes_paper/',
-          '~/NIH/eyeintegration_splicing/sampleTableV6.tsv', 
-          '~/NIH/occular_transcriptomes_paper/clean_data/EiaD_quant.Rdata',
-          '~/NIH/eyeintegration_splicing/dl_data/gfc_TCONS_to_st_MSTRG.tsv.gz',
-          '~/NIH/eyeintegration_splicing/all_tissues.combined.gtf',
-          '~/NIH/eyeintegration_splicing/dl_data/all_tissues.combined_transdecoderCDS.gff3.gz',
+args <- c('/Volumes/data/occular_transcriptomes_paper/',
+          '/Volumes/data/eyeintegration_splicing/sampleTableFull.tsv', 
+          '/Volumes/data/eyeintegration_splicing/data/all_tissue_quant.Rdata',
+          '/Volumes/data/eyeintegration_splicing/data/misc/TCONS2MSTRG.tsv',
+          '/Volumes/data/eyeintegration_splicing/data/gtfs/all_tissues.combined.gtf',
+          '/Volumes/data/eyeintegration_splicing/data/seqs/transdecoder_results/all_tissues.combined_transdecoderCDS.gff3',
+          '/Volumes/data/eyeintegration_splicing/ref/core_tight.Rdata',
           '~/NIH/occular_transcriptomes_paper/clean_data/fetal_novel_tx_diffexp_results.Rdata',
           '~/NIH/occular_transcriptomes_paper/clean_data/fetal_novel_tx_diffexp.txt',
           '~/NIH/occular_transcriptomes_paper/clean_data/fetal_novel_tx_diffexp_hm.Rdata'
           )
 
 args <- commandArgs(trailingOnly = T)
-
 wd <- args[1]
 sample_table_file <- args[2]
-eiad_quant_data <- args[3]
+salmon_quant_data <- args[3]
 t2m_file <- args[4]
 gtf_file <- args[5]
 gff3_file <- args[6]
-diffexp_data <- args[7]
-diff_exp_tx <- args[8]
-heatmap_file <- args[9]
+eiad_core_tight <- args[7]
+diffexp_data <- args[8]
+diff_exp_tx <- args[9]
+heatmap_file <- args[10]
 
 setwd(wd)
 
 sample_table <- read_tsv(sample_table_file)
-load(eiad_quant_data)
+load(salmon_quant_data)
+all_quant[is.na(all_quant)] <- 0
 tcons2mstrg <- read_tsv(t2m_file)
 gtf <- rtracklayer::readGFF(gtf_file)
 gff3 <- rtracklayer::readGFF(gff3_file) %>% 
@@ -37,42 +39,31 @@ gff3 <- rtracklayer::readGFF(gff3_file) %>%
 retina_fetal_exp <- tcons2mstrg %>% select(transcript_id, Retina_Fetal.Tissue) %>% 
     filter(!is.na(Retina_Fetal.Tissue)) %>%
     pull(transcript_id)
-
-
-samples <- gene_quant$sample_accession
-fetal_gene_quant <- gene_quant[,-1] %>% t() %>% as.data.frame %>% mutate(gene_name=rownames(.)) %>% 
-    select(gene_name, everything())
-colnames(fetal_gene_quant) <- c('gene_name', samples)
-
-samples <- tx_quant$sample_accession
-fetal_tx_quant <- tx_quant[,-1] %>% t() %>% as.data.frame %>%
-    mutate(transcript_id=str_extract(rownames(.), '\\((.*?)\\)') %>% str_remove('\\(') %>% str_remove('\\)'), 
-           gene_name=str_remove(rownames(.), '\\((.*?)\\)')) %>% select(transcript_id,gene_name, everything())
-colnames(fetal_tx_quant) <- c('transcript_id','gene_name', samples)
-
-
-core_tight_fetal <- filter(core_tight_2019, sample_accession %in% colnames(fetal_gene_quant)) %>% 
+retina_fetal_samples <- sample_table %>% filter(subtissue == 'Retina_Fetal.Tissue') %>% pull(sample)
+fetal_tx_quant <- all_quant %>% filter(transcript_id %in% retina_fetal_exp) %>% select(transcript_id,retina_fetal_samples)
+load(eiad_core_tight)
+core_tight_fetal <- filter(core_tight, sample_accession %in% colnames(fetal_tx_quant), Tissue == 'Retina', Kept=='Kept') %>% 
     select(colnames(.)[1:6], study_accession) %>%
     mutate(Sub_Tissue= ifelse(Sub_Tissue =="Retina - Fetal Tissue", 'Retina_Fetal.Tissue', 'Retina_Organoid'),
-           Age_Days=as.numeric(Age_Days))
+           Age_Days=as.numeric(Age_Days))# there's a rogue RPE sample marked as retina organoid
+fetal_tx_quant <- fetal_tx_quant %>% select(transcript_id, core_tight_fetal$sample_accession)
 colnames(core_tight_fetal) <- c('sample', 'tissue', 'subtissue', 'origin', 'age', 'kept', 'study_accession')
-fetal_tissue_samples <- core_tight_fetal %>% filter(subtissue == 'Retina_Fetal.Tissue', kept=='Kept') %>% arrange(age)
-organoid_samples <- core_tight_fetal %>% filter(subtissue == 'Retina_Organoid', kept=='Kept') %>% arrange(age)
+fetal_tissue_samples <- core_tight_fetal %>% filter(subtissue == 'Retina_Fetal.Tissue') %>% arrange(age)
+organoid_samples <- core_tight_fetal %>% filter(subtissue == 'Retina_Organoid') %>% arrange(age)
 
 
 library(limma)
 library(edgeR)
 #gdata::keep(fetal_tissue_samples, fetal_gene_quant)
 sample_design <- fetal_tissue_samples %>% select(sample, age, study_accession) %>%
-    mutate(stage= case_when(age <=84 ~ 'early',
-                            age >84 & age<119 ~ 'mid', 
-                            age >=119 ~ 'late'))
+    mutate(stage= case_when(age <=107 ~ 'early',
+                            age >107 ~ 'late'))
 table(sample_design$stage)
 sample_design %>% group_by(stage) %>% summarise(range=max(age) - min(age))
 
-gene_names <- fetal_gene_quant$gene_name
-exp_mat <- fetal_gene_quant %>% .[,sample_design$sample] 
-rownames(exp_mat) <- gene_names
+tx_names <- fetal_tx_quant$transcript_id
+exp_mat <- fetal_tx_quant %>% .[,sample_design$sample] 
+rownames(exp_mat) <- tx_names
 keep <- rowSums(exp_mat) > 5*ncol(exp_mat)
 exp_mat <- exp_mat[keep,]
 nrow(exp_mat)
@@ -99,52 +90,37 @@ design.pairs <-function(levels) {
     design
 }
 
-cont_mat <- design.pairs(c(levels(stage),levels(study))) %>% .[,grepl('early|mid|late', colnames(.))]
+cont_mat <- design.pairs(c(levels(stage),levels(study))) %>% .[,grepl('early|late', colnames(.))]
 model_fitted <-lmFit(voom_dge, design = design_mat)
 cont_mat <- cont_mat[colnames(model_fitted),]
 model_results <- contrasts.fit(model_fitted, cont_mat) %>% eBayes
 colnames(model_results)
-targets <- c("early-late","early-mid", 'late-mid')
-upregulated <- list(early=c(), mid=c(), late=c())
-limma_de <- list()
+targets_cont <- topTable(model_results, adjust.method = 'fdr', coef = "early-late", number = 30000000, p.value = .05)
+sum(rownames(targets_cont) %in% novel_loci_distinct$transcript_id)
+upregulated <-  target_cont %>% filter(logFC >=1)
+downregulated <- target_cout %>% filter(logfc<=-1)
 
-for( i in c("early-late","early-mid", 'late-mid')){
-    conds <- str_split(i, '-')[[1]]
-    top <- conds[1]
-    bottom <- conds[2]
-    df <-  topTable(model_results, adjust.method = 'fdr', coef = i, number = 30000000, p.value = .05) %>% 
-        as.data.frame %>% mutate(gene_name=rownames(.)) %>% 
-        filter(grepl('DNTX', gene_name), gene_name %in% retina_fetal_exp)
-    upregulated[[top]] <- c(upregulated[[top]],
-                            df %>% filter(logFC >2) %>% arrange(desc(logFC)) %>%  pull(gene_name))
-    upregulated[[bottom]] <- c(upregulated[[bottom]], 
-                               df %>% filter(logFC <2) %>% arrange(desc(abs(logFC))) %>% pull(gene_name) )
-    limma_de[[i]] <- df
-}
-
-shared_upregulated <- reduce(upregulated, intersect)  
-distinct_upregulated <- combine(upregulated) %>% unique 
 des <- model.matrix(~0 + stage)
 batch_cor_exp <- removeBatchEffect(exp_mat, study, design = des)
 batch_cor_exp[batch_cor_exp<0] <- 0
 
-
-
-
-mat <- batch_cor_exp[distinct_upregulated,] 
-exp_by_stage <- lapply(c('early', 'mid', 'late'), function(x) filter(sample_design, stage == x) %>% pull(sample) %>% {batch_cor_exp[,.]} %>%
-           rowMeans) %>% bind_cols %>% mutate()
-
-
-sum(mat>500)/length(mat)
-mat[mat>500] <- 500
+novel_loci <- filter(gtf, grepl('DNTX', gene_name), type == "transcript") 
+sum(rownames(targets_cont) %in% novel_loci_distinct$transcript_id)
+keep <- rownames(batch_cor_exp) %in% novel_loci_distinct$transcript_id & rownames(batch_cor_exp) %in% rownames(targets_cont)
+mat <- log2(batch_cor_exp[keep,]+1)
+# exp_by_stage <- lapply(c('early', 'mid', 'late'), function(x) filter(sample_design, stage == x) %>% pull(sample) %>% {batch_cor_exp[,.]} %>%
+#            rowMeans) %>% bind_cols %>% mutate()
+# 
+# 
+# sum(mat>500)/length(mat)
+# mat[mat>500] <- 500
 topAno <- HeatmapAnnotation(age=sample_design$age, 
                             stage=sample_design$stage, 
                             study=sample_design$study_accession,
-                            col=list(stage=c('early'='yellow', 'mid'='orange', 'late'='red'),
-                                     study=c("SRP119766"='green', "SRP105756"='purple', "SRP090040"='blue')),
+                            col=list(stage=c('early'='yellow', 'late'='red'),
+                                     study=c("SRP119766"='green',  "SRP090040"='blue')),
                             which = 'col')
-rightAno <- HeatmapAnnotation(protein_coding=ifelse(distinct_upregulated %in% gff3$ID, 'pc', 'nc'),
+rightAno <- HeatmapAnnotation(protein_coding=ifelse(rownames(mat)%in% gff3$ID, 'pc', 'nc'),
                               col=list(protein_coding=c('pc'='pink', 'nc'='white')),
                               which = 'row')
 
