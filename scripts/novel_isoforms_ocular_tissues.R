7e7b104d0a696e3713bb62d4cfe89cb4e4546c02
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-05-21 17:14:05 -0400

    code added for principal isoform fig

--- scripts/novel_isoforms_ocular_tissues.R
+++ scripts/novel_isoforms_ocular_tissues.R
@@ -13,9 +13,9 @@ parser$add_argument('--filesYaml', action = 'store', dest = 'files_yaml')
 list2env(parser$parse_args(), .GlobalEnv)
 
 ####
-# working_dir <- '/data/swamyvs/ocular_transcriptomes_paper/'
-# data_dir <- '/data/swamyvs/ocular_transcriptomes_pipeline/'
-# files_yaml <- '/data/swamyvs/ocular_transcriptomes_paper/files.yaml'
+working_dir <- '/data/swamyvs/ocular_transcriptomes_paper/'
+data_dir <- '/data/swamyvs/ocular_transcriptomes_pipeline/'
+files_yaml <- '/data/swamyvs/ocular_transcriptomes_paper/files.yaml'
 ####
 files <- read_yaml(files_yaml)
 setwd(working_dir)
@@ -26,6 +26,7 @@ gtf <- rtracklayer::readGFF(files$anno_gtf)
 anno_tab <- gtf %>% filter(type == "transcript") %>% select(transcript_id, gene_name, oId)
 tc2ms_all <- fread(files$tcons2mstrg) %>% as_tibble
 sample_table <- read_tsv(files$sample_table)
+subtissues <- unique(sample_table$subtissue)
 tc2ms <- tc2ms_all %>% filter(!transcript_id %in% novel_loci_distinct$transcript_id) 
 eye_tissues <- c('Retina_Fetal.Tissue', 'Retina_Adult.Tissue', 'RPE_Fetal.Tissue', 'RPE_Adult.Tissue',
                  'Cornea_Adult.Tissue', 'Cornea_Fetal.Tissue')
@@ -63,13 +64,6 @@ novel_eye_tx_by_tissue <- novel_tx_by_tissue
 
 novel_tx_by_tissue[['Brain(all)']] <- novel_brain_tx_det$transcript_id
 novel_tx_by_tissue[['Body(all)']] <- novel_body_det$transcript_id
-#upset(fromList(novel_eye_tx_by_tissue),nintersects = 20, nsets = length(novel_eye_tx_by_tissue), order.by = 'freq')
-
-
-
-
-# piu plots 
-
 
 sample_table_eye <- sample_table %>% filter(subtissue %in% eye_tissues)
 
@@ -80,28 +74,28 @@ t2g <- gtf %>%
 load(files$all_tissue_quant)
 all_quant[is.na(all_quant)] <- 0
 counts_eye <- all_quant[,c('transcript_id', sample_table_eye$sample)]
-counts_eye_by_tissue <- lapply(eye_tissues,
-                               function(tis) filter(sample_table_eye, subtissue == tis) %>% pull(sample) %>%
-                                   {counts_eye[,c('transcript_id', .)]} %>%
+counts_by_tissue <- lapply(subtissues,
+                               function(tis) filter(sample_table, subtissue == tis) %>% pull(sample) %>%
+                                   {all_quant[,c('transcript_id', .)]} %>%
                                    mutate(!!tis := rowMedians(.[,-1] %>% as.matrix)) %>%
                                    select(transcript_id, !!tis)
                                    ) %>%
-    reduce(left_join) %>% left_join(t2g, .)
+    reduce(full_join) %>% left_join(t2g, .)
 
-keep <- rowSums(counts_eye_by_tissue[,-(1:2)]) > 0
-med_0_counts_eye <- counts_eye_by_tissue[!keep,]
-counts_eye_by_tissue <- counts_eye_by_tissue[keep,] %>% distinct
+keep <- rowSums(counts_by_tissue[,-(1:2)]) > 0
+med_0_counts <- counts_by_tissue[!keep,]
+counts_by_tissue <- counts_by_tissue[keep,] %>% distinct
 
 
 
-long_counts_with_codes <- counts_eye_by_tissue %>% 
-    inner_join(tc2ms_all %>% select(transcript_id, class_code), .) %>% 
-    gather(key = 'subtissue', value = 'counts', -transcript_id, -class_code, -gene_name) %>% 
-    mutate(log2counts = log2(counts + 1), 
-           transcript_novelty = case_when(transcript_id %in% all_novel_tx ~ 'novel_isoforms', 
-                                         class_code == 'u' ~ 'novel Loci',
-                                         class_code == '='~ 'ref_isoform'
-                                         ))
+# long_counts_with_codes <- counts_eye_by_tissue %>% 
+#     inner_join(tc2ms_all %>% select(transcript_id, class_code), .) %>% 
+#     gather(key = 'subtissue', value = 'counts', -transcript_id, -class_code, -gene_name) %>% 
+#     mutate(log2counts = log2(counts + 1), 
+#            transcript_novelty = case_when(transcript_id %in% all_novel_tx ~ 'novel_isoforms', 
+#                                          class_code == 'u' ~ 'novel Loci',
+#                                          class_code == '='~ 'ref_isoform'
+#                                          ))
 # ggplot(long_counts_with_codes) +
 #     geom_boxplot(aes(x = transcript_novelty, y= log2counts, fill = subtissue) )+
 #     facet_wrap(~subtissue)
@@ -109,7 +103,7 @@ long_counts_with_codes <- counts_eye_by_tissue %>%
 
 
 calc_isoform_percentage <- function(t_tissue){
-    df <- counts_eye_by_tissue %>% select(transcript_id, gene_name, !!t_tissue)
+    df <- counts_by_tissue %>% select(transcript_id, gene_name, !!t_tissue)
     tt_sym <- as.symbol(t_tissue)
     df_gene_sums <- df %>% 
         select(-transcript_id) %>% 
@@ -132,12 +126,45 @@ replace_nan <- function(df) {
 
 
 
-piu_raw <- lapply(colnames(counts_eye_by_tissue)[-(1:2)], calc_isoform_percentage) %>% reduce(left_join)
+piu_raw <- lapply(colnames(counts_by_tissue)[-(1:2)], calc_isoform_percentage) %>% reduce(full_join)
+piu_all <-replace_nan(piu_raw)
+piu <- piu_all %>% select(transcript_id, gene_name, all_of(eye_tissues) )
+#### now identify primary isoforms for each tissue
+appris_tags <- fread(files$ref_gtf, header= F ) %>% 
+    as_tibble %>% 
+    mutate(appris_tag = str_extract(V9, "appris_\\w+_\\d+"), 
+           transcript_id = str_extract(V9,"transcript_id \"\\w+\\d+\\.\\d+\";") %>% str_split('"') %>% sapply(function(x)x[2])) %>% 
+    filter(V3 == 'transcript') %>% select(transcript_id, appris_tag)
 
-piu <-replace_nan(piu_raw)
+dntx2enst <- filter(tc2ms_all, class_code == '=') %>% select(dntx_id = transcript_id, transcript_id = refid)
 
 
-save.image('testing/novel_iso_ws.rdata')
+appris_tags_clean <- filter(appris_tags, !is.na(appris_tag)) %>% 
+    mutate(tag_level = str_split(appris_tag, '_') %>% sapply(function(x) x[3]) %>% as.numeric, 
+           tag_level = case_when(grepl('alternative', appris_tag) & tag_level == 1 ~ 6, 
+                                 grepl('alternative', appris_tag) & tag_level == 2 ~ 7,
+                                 TRUE ~ tag_level), 
+    )
+table(appris_tags_clean$tag_level)
+appris_in_dntx <-  inner_join(dntx2enst, appris_tags_clean)
+table(appris_in_dntx$tag_level)
+nrow(appris_in_dntx %>% filter(tag_level <6)) / nrow(appris_tags_clean %>% filter(tag_level <6))
+tx2code <- tc2ms_all %>% select(transcript_id, class_code)
+primary_isoforms_per_tissue <- piu_all %>% 
+    gather(key = 'subtissue', value = 'piu', -transcript_id, -gene_name) %>% 
+    group_by(subtissue, gene_name) %>% 
+    summarise(primary_iso = max(piu), transcript_id  = transcript_id[which.max(piu)]) %>% 
+    ungroup %>% 
+    filter(primary_iso >0) %>% 
+    mutate(is_appris = transcript_id %in% appris_in_dntx$dntx_id) %>% 
+    inner_join(tx2code) %>% 
+    mutate(primary_isoform_origin = case_when(is_appris ~ 'appris',  
+                                              !is_appris & class_code == '=' ~ 'gencode', 
+                                              !is_appris & class_code != '=' ~ 'dntx')) %>% 
+    group_by(subtissue, primary_isoform_origin) %>% 
+    summarise(count = n())
+
+
 summarise_novel_transcripts_in_tissue <- function(t_tissue) {
 
     
@@ -162,14 +189,14 @@ summarise_novel_transcripts_in_tissue <- function(t_tissue) {
         pull(transcript_id) %>% 
         {filter(piu, transcript_id %in% .)} %>% 
         select(transcript_id, piu := !!adult) %>% 
-        mutate(tissue=t_tissue, stage='adult')
+        mutate(tissue=t_tissue, stage='adult',subtissue =adult  )
     
     fetal_piu_spec <- tissue_spec_det %>% 
         filter(.[,fetal]) %>% 
         pull(transcript_id) %>% 
         {filter(piu, transcript_id %in% .)} %>% 
         select(transcript_id, piu := !!fetal) %>% 
-        mutate(tissue=t_tissue, stage='fetal')
+        mutate(tissue=t_tissue, stage='fetal', subtissue = fetal)
     
     list(loc_df <- tibble(),
          #loc_df=bind_rows(adult_locations, fetal_locations) %>% mutate(age=c('adult', 'fetal'), tissue=t_tissue) ,
@@ -182,8 +209,14 @@ res <- lapply(tissues, summarise_novel_transcripts_in_tissue)
 
 
 location_df <- lapply(res, function(x) x[['loc_df']]) %>% bind_rows #%>% gather(location, count, -age, -tissue)
-piu_df <- lapply(res, function(x) x[['piu_df']]) %>% bind_rows
-save(novel_tx_by_tissue, novel_eye_tx_by_tissue, piu_df, location_df, file = files$novel_isoform_analysis_rdata)
+long_eye_det <- det_mat %>% select(transcript_id, all_of(eye_tissues)) %>% 
+    filter(rowSums(.[,-1]) >0) %>% 
+    gather(key = 'subtissue', value = 'det', -transcript_id)
+
+piu_df <- lapply(res, function(x) x[['piu_df']]) %>% bind_rows %>% inner_join(long_eye_det) %>% filter(det)
+
+
+save(novel_tx_by_tissue, novel_eye_tx_by_tissue, piu_df, location_df, primary_isoforms_per_tissue, file = files$novel_isoform_analysis_rdata)
 
 
 
