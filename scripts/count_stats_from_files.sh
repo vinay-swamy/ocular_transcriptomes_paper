99bf7f267416e3f014258c335bb1a80d91bc4ace
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-05-27 17:28:34 -0400

    removed hardcoded paths

--- scripts/count_stats_from_files.sh
+++ scripts/count_stats_from_files.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 sample_table=$1
-quant_path=$2
-DNTX_mapping_rates=$3
-gencode_mappingrates=$4
-gtfs_counts=$5
-rm -f $3 $4 $5
-subtissues=`cut ${sample_table} -f4 | sort -u `
+data_dir=$2
+dntx_quant_path=$3
+DNTX_mapping_rates=$4
+gencode_mappingrates=$5
+gtfs_counts=$6
+rm -f $4 $5 $6
+subtissues=`cut ${sample_table} -f4 | tail -n+2  |sort -u `
 
 for subt in $subtissues
 do
-    for sample in ${quant_path}/data/salmon_quant/dntx/${subt}/*/aux_info/meta_info.json
+    for sample in ${dntx_quant_path}/${subt}/*/aux_info/meta_info.json
         do
             echo $sample `grep num_processed $sample ` `grep percent_mapped $sample` >> ${DNTX_mapping_rates}
         done
 done
 
 
-for sample in ${quant_path}/gencode_quant/quant_files/*/aux_info/meta_info.json
+for sample in ${dntx_quant_path}/gencode/*/aux_info/meta_info.json
     do
         echo $sample `grep num_processed $sample ` `grep percent_mapped $sample` >> ${gencode_mappingrates}
     done
 
 
-for gtf in ${quant_path}/data/gtfs/*/*.gtf
+for gtf in ${data_dir}/data/gtfs/*/*.gtf
     do
         echo $gtf `awk '$3 == "transcript"' $gtf | wc -l` >> ${gtfs_counts}
     done
