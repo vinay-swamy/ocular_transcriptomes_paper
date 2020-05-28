32e0fd9cfd82c2f31ed45f957066a22bd390c820
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-05-28 17:21:12 -0400

    now only paper numbers, have filled in a lot of the missing numbers

--- scripts/paper_numbers_sup_figs.R
+++ scripts/paper_numbers_sup_figs.R
@@ -10,8 +10,8 @@ parser$add_argument('--filesYaml', action = 'store', dest = 'files_yaml')
 list2env(parser$parse_args(), .GlobalEnv)
 
 ###
-# working_dir <- '/data/swamyvs/ocular_transcriptomes_paper/'
-# files_yaml <- '/data/swamyvs/ocular_transcriptomes_paper/files.yaml'
+working_dir <- '/data/swamyvs/ocular_transcriptomes_paper/'
+files_yaml <- '/data/swamyvs/ocular_transcriptomes_paper/files.yaml'
 ###
 
 
@@ -39,7 +39,6 @@ NUM_NOVEL_TX_BODY <- pan_body_gtf %>% filter( type == 'transcript', class_code!=
 NUM_NOVEL_TX_EYE  <- pan_eye_gtf  %>% filter( type == 'transcript', class_code!='=')%>% nrow 
 NUM_TOTAL_TX_BODY <- NUM_NOVEL_TX_BODY + NUM_NOVEL_TX_BODY
 NUM_TOTAL_TX_BODY <- NUM_NOVEL_TX_EYE + NUM_NOVEL_TX_EYE
-
 load(files$ref_tx_exon_rdata)
 all_exon_bed <- all_exons %>% mutate(score= 888) %>% select(seqid, start, end, origin, score, strand) %>% 
     from_data_frame %>% 
@@ -56,12 +55,38 @@ NUM_TOTAL_NOVEL_SEQ <- novel_sequence %>% mutate(length=X3-X2) %>% pull(length) 
 cds_origin_df <- fread(files$CDS_origin_df)
 NUM_REF_ORF <-  cds_origin_df %>% select(-transcript_id) %>% distinct %>% pull(is_annotaed_cds) %>% {sum(.)}
 NUM_NOVEL_ORF <- cds_origin_df %>% select(-transcript_id) %>% distinct %>% pull(is_annotaed_cds) %>% {sum(!.)}
+########
 
+######## Txome size
+load(files$txome_stats_rdata)
+NUM_AVG_BASE_TXOME_SIZE = mean(gencode_tx_size$all_exp) %>% round(digits = 2)
+size_red <- inner_join(gencode_tx_size, tx_counts) %>% mutate(frac_decrease = (all_exp - filtered)/all_exp)
+NUM_AVG_TXOME_SIZE_DECREASE = mean(size_red$frac_decrease) %>% round(digits = 5)
+NUM_AVG_MAP_RATE_DIF = mean(all_sample_mapping_rate_difference$mapping_rate_diff) %>% round(digits = 5)
+########
+######## Tx counts 
+conv_tab <- fread(files$tcons2mstrg)
+cds_origin <- fread(files$CDS_origin_df)
+long_det_df <- apply(conv_tab[,-(1:4)],2, function(col) col!='') %>% as_tibble %>% bind_cols(conv_tab[,1:4], .) %>% 
+    select(-refid, -gene_id) %>% gather(key = 'subtissue', value = 'det', -transcript_id,-class_code ) %>% 
+    left_join(cds_origin)
+tx_count_by_tissue <- long_det_df %>%
+    mutate(class_code = replace(class_code,!class_code %in% c('=', 'u'), 'N')) %>% 
+    group_by(subtissue, class_code) %>% 
+    summarise(count = sum(det))
+NUM_AVG_REF_TX_PER_TISSUE = tx_count_by_tissue %>% filter(class_code == '=') %>% pull(count) %>% mean %>% round(digits = 2)
+NUM_AVG_NOVEL_ISOFORM_PER_TISSUE = tx_count_by_tissue %>% filter(class_code == 'N') %>% pull(count) %>% mean %>% round(digits = 2)
+NUM_AVG_NOVEL_LOCI_PER_TISSUE = tx_count_by_tissue %>% filter(class_code == 'u') %>% pull(count) %>% mean %>% round(digits = 2)
 
+orf_count_per_tissue <- long_det_df %>% filter(det, !is.na(cds_id)) %>% 
+    select(subtissue, cds_id, is_annotaed_cds) %>% 
+    distinct %>% 
+    group_by(subtissue) %>% 
+    summarise(count = sum(!is_annotaed_cds))
+NUM_AVG_NOVEL_ORF_PER_TISSUE <- mean(orf_count_per_tissue$count) %>% round(digits = 2)
 
-
-
-####long read data numbers 
+########
+########long read data numbers 
 load(files$long_read_results_rdata)
 NUM_BASE_TXOME_CONST_ACC <- acc_df_alltx %>% filter(build == 'stringtie') %>% pull(accuracy) %>% round(digits = 3)
 NUM_TXOME_CONST_ACC_2000_ABOVE <- acc_df_by_length %>% filter(as.numeric(qbin) >2000) %>% pull(stringtie_acc) %>% mean %>% round(digits = 3)
@@ -83,7 +108,13 @@ appris_totals <- primary_isoforms_per_tissue %>%
 NUM_AVG_APPRIS <- mean(appris_totals$percent_appris)
 NUM_AVG_NONAPPRIS_GENCODE = mean(appris_totals$percent_gencode)
 NUM_AVG_NOVEL = mean(appris_totals$percent_novel)
-
+#######
+####### VEP numbers
+load(files$variant_results_rdata)
+impact_diff_changes <- impact_diff %>% filter(min_diff!=0 | max_diff!=0)
+NUM_TOTAL_VUS <- nrow(impact_diff)
+NUM_IMPACT_INCREASE <- impact_diff_changes %>% filter(max_diff>0) %>% nrow
+NUM_IMPACT_DECREASE <- impact_diff_changes %>% filter(min_diff > 0) %>% nrow 
 ####### Correlation between expression and transcript length 
 load(files$gencode_quant)
 subtissues <- unique(sample_table$subtissue)
@@ -105,6 +136,8 @@ calc_txome_size <- function(t_tissue){
 }
 res <- sapply(subtissues, calc_txome_size)
 NUM_AVG_EXP_TX_LENGTH_COR <- mean(res)
+############
+
 
 save(list= ls()[grepl('NUM_', ls())], file = files$paper_numbers_rdata)
 
