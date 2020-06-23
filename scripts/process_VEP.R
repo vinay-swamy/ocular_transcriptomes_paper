3d42497852e978f5b8bdde54c75b4f02eadb9c10
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2020-06-23 14:03:47 -0400

    the generating transcript models part is still a little rough but works

--- scripts/process_VEP.R
+++ scripts/process_VEP.R
@@ -5,6 +5,7 @@ library(argparse)
 library(yaml)
 library(RBedtools)
 library(parallel)
+library(glue)
 parser <- ArgumentParser()
 parser$add_argument('--dataDir', action = 'store', dest = 'data_dir')
 parser$add_argument('--filesYaml', action = 'store', dest = 'files_yaml')
@@ -19,11 +20,12 @@ setwd(data_dir)
 
 
 clinvar_anno <- fread(files$clinvar_summary, sep = '\t') %>% 
-    as_tibble %>% rename(allele_id = `#AlleleID`) 
+    as_tibble %>% rename(allele_id = `#AlleleID`, 
+                         var_id = VariationID) 
 clinvar_vus <- clinvar_anno %>% filter(ClinicalSignificance == "Uncertain significance")
 clinvar_vus_eye <- clinvar_vus %>% filter(grepl('macula|retin|leber|cone|cornea|bardet|ocular|optic|ocular|joubert', PhenotypeList))
 
-VUS_ids <-clinvar_vus  %>% pull(allele_id) %>% unique
+VUS_ids <-clinvar_vus  %>% pull(var_id) %>% unique
 clinsig_rank <- tibble(IMPACT = c('HIGH', 'MODERATE', 'LOW', "MODIFIER")) %>%  
     mutate(impact_code  = case_when(IMPACT == 'HIGH' ~ 3, 
                                     IMPACT == 'MODERATE' ~2, 
@@ -86,27 +88,27 @@ read_VEP <- function(file, tissue, c_med_t_tpm, c_avg_t_tpm){
 vep_files <- paste0(files$VEP_dir, subtissues,'/variant_summary.txt')
 all(file.exists(vep_files))
 dntx_vep_by_tissue <- lapply(seq_along(subtissues), function(i)read_VEP(vep_files[i], subtissues[i],med_dntx_tissue_tpm, avg_dntx_tissue_tpm )) %>%
-    bind_rows
+    bind_rows %>% rename(var_id = allele_id)
 gencode_vep_file <- paste0(files$VEP_dir, 'gencode/variant_summary.txt')
 #slow but i lazy
 gencode_vep_by_tissue <- lapply(seq_along(subtissues), function(i)read_VEP(gencode_vep_file, subtissues[i],med_gencode_tissue_tpm, avg_gencode_tissue_tpm )) %>%
-    bind_rows
+    bind_rows %>% rename(var_id = allele_id)
 
 save.image('testing/process_vep.Rdata')
 dntx_global_impacts <- dntx_vep_by_tissue %>% 
-    group_by(allele_id) %>% 
+    group_by(var_id) %>% 
     summarise(g_max_impact = max(max_impact), 
               g_min_impact = min(max_impact))
 
 
 gencode_global_impacts <- gencode_vep_by_tissue %>% 
-    select(allele_id, gencode_impact=max_impact) %>% distinct 
+    select(var_id, gencode_impact=max_impact) %>% distinct 
 impact_diff <- inner_join(dntx_global_impacts, gencode_global_impacts) %>% 
     mutate(max_diff = g_max_impact - gencode_impact, min_diff = gencode_impact - g_min_impact )
 
 
 dntx_vep_impact_matrix <- dntx_vep_by_tissue %>% 
-    select(allele_id, subtissue, max_impact) %>% 
+    select(var_id, subtissue, max_impact) %>% 
     spread(key = subtissue, value = max_impact) %>% 
     mutate(one_count = rowSums(.[,subtissues] == 1), 
            two_count = rowSums(.[,subtissues] == 2), 
@@ -117,36 +119,43 @@ dntx_vep_impact_matrix[is.na(dntx_vep_impact_matrix)] <- 1
 
 n=10
 n=10
-vep_exp_all_eye <- filter(dntx_vep_impact_matrix, allele_id %in% clinvar_vus_eye$allele_id) %>%
+vep_exp_all_eye <- filter(dntx_vep_impact_matrix, var_id %in% clinvar_vus_eye$var_id) %>%
     filter(rowSums(.[,eye_tissues] == 3) >=1, rowSums(.[,body_tissues] !=3) ==length(body_tissues))
-vep_no_eye <- filter(dntx_vep_impact_matrix, allele_id %in% clinvar_vus_eye$allele_id) %>% 
-    filter(rowSums(.[,eye_tissues] != 3) == length(eye_tissues),three_count >4  ) 
+
+vep_full_priority_increase <- dntx_vep_impact_matrix %>% 
+    #filter( var_id %in% clinvar_vus_eye$var_id) %>%
+    inner_join(gencode_global_impacts)%>% 
+    filter(three_count>=1, gencode_impact==1  )
+
+    vep_no_eye <- filter(dntx_vep_impact_matrix, var_id %in% clinvar_vus_eye$var_id) %>%
+    inner_join(gencode_global_impacts) %>% 
+    filter(rowSums(.[,eye_tissues] == 1) == length(eye_tissues), gencode_impact == 3, three_count >0) %>% select(-gencode_impact)
 impact_diff_changes <- impact_diff %>% filter(min_diff!=0 | max_diff!=0)
-eye_impact_increase <- impact_diff_changes %>% filter(max_diff>0, allele_id%in% clinvar_vus_eye$allele_id)
-vep_impact_increase <- filter(dntx_vep_impact_matrix, allele_id %in% eye_impact_increase$allele_id)
+eye_impact_increase <- impact_diff_changes %>% filter(max_diff>0, var_id%in% clinvar_vus_eye$var_id)
+vep_impact_increase <- filter(dntx_vep_impact_matrix, var_id %in% eye_impact_increase$var_id)
 
 
 vep_example <- bind_rows( vep_exp_all_eye %>% arrange(desc(var))%>% 
-                              #.[1:n,] %>% 
-                              select(allele_id, eye_tissues, everything()),
+                              .[1:min(n,nrow(.)),] %>% 
+                              select(var_id, eye_tissues, everything()),
                           vep_no_eye %>% arrange(desc(var)) %>% 
-                              #.[1:n,] %>% 
-                              select(allele_id, eye_tissues, everything()) ,
+                              .[1:min(n,nrow(.)),] %>% 
+                              select(var_id, eye_tissues, everything()) ,
                           vep_impact_increase
 )
-clinvar_example <- filter(clinvar_vus_eye, allele_id %in% vep_example$allele_id) %>% select(allele_id, contains('pheno'))
-#vep_example <- filter(vep_impact_matrix, allele_id %in% clinvar_vus_eye$allele_id) %>% arrange(desc(var))
+clinvar_example <- filter(clinvar_vus_eye, var_id %in% vep_example$var_id) %>% select(var_id, contains('pheno'))
+#vep_example <- filter(vep_impact_matrix, var_id %in% clinvar_vus_eye$var_id) %>% arrange(desc(var))
 gencode_vep_by_tissue_simple <- gencode_vep_by_tissue %>% 
     mutate(subtissue = 'gencode', max_impact_avg_exp=1, max_impact_med_exp=1) %>% 
     distinct()
 merge_bt <- bind_rows(dntx_vep_by_tissue %>% mutate(build= 'dntx'),
                       gencode_vep_by_tissue_simple%>% mutate(build= 'gencode'))
 vep_plot_df <- merge_bt %>%  
-    filter(allele_id %in% vep_example$allele_id) %>%
+    filter(var_id %in% vep_example$var_id) %>%
     mutate(avg_exp = log2(max_impact_avg_exp+1), 
            med_exp = log2(max_impact_med_exp + 1),
            avg_exp = replace(avg_exp , avg_exp >5, 5),
-           allele_id = as.character(allele_id), 
+           var_id = as.character(var_id), 
            `Variant Impact` = case_when(max_impact == 1 ~'Low',
                                         max_impact == 2 ~ 'Moderate', 
                                         max_impact == 3 ~ 'High') %>% factor(levels=c('Low', 'Moderate', 'High')),
@@ -154,5 +163,86 @@ vep_plot_df <- merge_bt %>%
     ) %>% 
     rename(`log2(TPM + 1)` = avg_exp)
 vep_plot_df[is.na(vep_plot_df)] <- 0
+#####################################################################################################################################  
+#make 
 
-save(vep_plot_df, impact_diff, clinvar_vus_eye, file = files$variant_results_rdata)
+example_var_ids <- c(631672, 631715)
+gtf <- rtracklayer::readGFF(files$anno_gtf)
+
+write(example_var_ids %>% paste0('^',.), '/tmp/varids.txt', sep = '\b')
+read_VEP_ttx <- function(file, tissue){
+    cmd <- glue('grep -Ef /tmp/varids.txt {file}')
+    fread(cmd =  cmd, sep = '\t') %>% 
+        as_tibble %>% 
+        select(var_id = V1, transcript_id = V5, cons = V7, impact = V14 ) %>% 
+        left_join(med_dntx_tissue_tpm[[tissue]]) %>% 
+        # filter(allele_id %in% VUS_ids) %>% 
+        # select(allele_id,transcript_id = Feature,Consequence, Extra) %>% 
+        mutate(subtissue = tissue)
+    
+}
+
+dntx_ttx_bytissue <- lapply(seq_along(subtissues), function(i)read_VEP_ttx(vep_files[i], subtissues[i] )) %>%
+    bind_rows
+res <- dntx_ttx_bytissue %>% mutate(med_exp = replace_na(med_exp, 0)) %>% 
+    group_by(var_id, subtissue) %>% 
+    summarise(max_tpm = max(med_exp), 
+              transcript_id = transcript_id[which.max(med_exp)],
+              cons = cons[which.max(med_exp)], 
+              impact = impact[which.max(med_exp)])
+
+k <- filter()
+
+var_bed <- clinvar_vus_eye %>% 
+    filter(var_id%in% example_var_ids, Assembly == 'GRCh38') %>% 
+    select(Chromosome, Start, Stop, var_id) %>%
+    mutate(Chromosome = paste0('chr', Chromosome)) %>% 
+    from_data_frame
+exon2var <- gtf %>% 
+    filter(type == 'exon') %>% 
+    select(seqid, start, end, transcript_id) %>% 
+    from_data_frame %>% 
+    RBedtools('sort', output = 'stdout', i=.) %>% 
+    RBedtools('intersect', options = '-wa -wb', a=.,b=var_bed) %>% 
+    to_data_frame()
+    
+conv_tab <- fread(files$tcons2mstrg)  
+refid2dntx <- conv_tab %>% mutate(new_id = replace(transcript_id, class_code == '=',refid[class_code == '='])) %>% 
+    select(transcript_id, new_id)
+
+
+t_var <- 631672
+t_gene <- "CACNA2D4"
+other_tx <- gtf%>% filter(gene_name ==t_gene) %>% select(transcript_id) %>% inner_join(conv_tab) %>% 
+    select(-contains('Retina'), -RPE_Fetal.Tissue) %>% filter(rowSums(.[,-(1:4)]!='') >0) %>% distinct %>%
+    filter(class_code == '=') %>% pull(transcript_id)
+hit_tx <- filter(res, var_id == t_var, transcript_id != '-') %>% pull(transcript_id)
+all_tx <- c(hit_tx, other_tx)
+
+all_tx_cleanids <- filter(refid2dntx, transcript_id %in% all_tx) %>% 
+    select(transcript_id, new_id) %>% inner_join(tibble(transcript_id = all_tx),.) %>% pull(new_id)
+ctab_plotting <- filter(conv_tab, transcript_id %in% other_tx) %>% 
+    select(-refid, -class_code, -gene_id, -contains('Retina')) %>% 
+    gather(key = 'tissue', value = 'oId', -transcript_id) %>% filter(oId != '') %>% 
+    group_by(transcript_id) %>% summarise(subtissue = first(tissue)) %>%
+    bind_rows( filter(res %>% ungroup, var_id == t_var, transcript_id != '-') %>% select(transcript_id, subtissue)) %>% 
+    inner_join(refid2dntx) %>% select(-transcript_id) %>% rename(transcript_id = new_id)
+
+library(DBI)
+con <-  dbConnect(RSQLite::SQLite(), 'data/shiny_data/app_data/DNTX_db.sql')
+pg <- con %>% tbl('plotting_gtf') %>% filter(gene_name == t_gene) %>% collect  #%>% filter(transcript_id%in%k$transcript_id)
+pg_ctx <- pg %>% filter(transcript_id %in% all_tx_cleanids)
+
+
+var_plot <- filter(exon2var, X4 == all_tx[1], X8 ==t_var) %>% 
+    select(seqid = X1, start = X2, end = X3, transcript_id = X4, var_start = X6, var_end = X7, var_id = X8) %>% 
+    inner_join(refid2dntx) %>% select(-transcript_id) %>% rename(transcript_id = new_id) %>% 
+    inner_join(pg_ctx) %>% mutate(Xmin = Xmin + (var_start - start), Xmax = Xmin +1) %>% 
+    select(seqid, start, end, transcript_id,  var_x = Xmin,var_y = Ymax)
+
+save(vep_plot_df, pg_ctx, all_tx_cleanids,ctab_plotting,var_plot,
+     example_var_ids, impact_diff, clinvar_vus_eye, file = files$variant_results_rdata)
+
+
+dbDisconnect(con)
+
